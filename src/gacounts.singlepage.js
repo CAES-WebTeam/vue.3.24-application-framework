2d172ddbe8976be49731e707868359f03175ca54
scott_allen <scott.allen@uga.edu>
2023-06-13 07:17:53 -0400

    init

--- src/gacounts.singlepage.js
+++ src/gacounts.singlepage.js
@@ -1,15 +1,16 @@
-/// Converted - Polyfill for URL api and IE11
+// Converted - Polyfill for URL api and IE11
 import 'url-polyfill';
 
-/// Converted - Polyfill for Element.matches and IE11
+// Converted - Polyfill for Element.matches and IE11
 import 'element-matches';
 
-// COnverted - Importing createApp from the Vue package: 
-//The createApp function is used to create a Vue application instance in Vue 3.
+// Converted - Importing createApp from the Vue package: 
+// The createApp function is used to create a Vue application instance in Vue 3.
 import { createApp } from 'vue'; 
 import GACountsReport from '~/GACountsReport';
 import { logError } from '~/modules/caesdb';
 import dateFormat from 'dateformat';
+import { getComputed, getMapState, getStore, getStoreConfig } from '~/modules/store';
 
 /* Converted SA Vue 3 - Creating the Vue application instance: 
 Instead of using new Vue(), use createApp() to create the Vue application instance. */
@@ -18,20 +19,28 @@ const app = createApp(GACountsReport);
 app.config.errorHandler = logError;
 app.config.warnHandler = logError;
 
-   /* Converted SA - Filters: In Vue 3, filters are no longer available as built-in features.
-   similar functionality using methods or computed properties. the app.filter() calls 
-   have been replaced with custom methods or computed properties to handle the same logic. */
-app.filter('capitalize', str => { 
-  if (!str) return '';
-  str = str.toString();
-  return str.charAt(0).toUpperCase() + str.slice(1);
-});
+/* Converted SA - Filters: In Vue 3, filters are no longer available as built-in features.
+similar functionality using methods or computed properties. the app.filter() calls 
+have been replaced with custom methods or computed properties to handle the same logic. */
 
-
-app.filter('simple-date', dt => dateFormat(dt, 'm/d/yy')); 
+app.config.globalProperties.$filters = {
+  capitalize: function (str) {
+    if (!str) return '';
+    str = str.toString();
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  },
+  simpleDate: function(dt) {
+    return dateFormat(dt, 'm/d/yy');
+  }
+}
 
 /* Converted - Mounting the application: Instead of using el property in the Vue instance to mount 
 the application, use the mount() method on the Vue application instance to specify the element 
 where the application should be mounted. In this case, app.mount('#app') mounts the application to 
 the element with the ID "app" */
 app.mount('#app'); 
+
+// Assuming you have a schema to pass into getStore and getStoreConfig functions
+let schema = {}; // replace this with your actual schema
+let storeConfig = getStoreConfig(schema);
+let store = getStore(schema);
--- src/views/custom/gacounts3/DuplicationPane4HEnrollment.vue
+++ src/views/custom/gacounts3/DuplicationPane4HEnrollment.vue
@@ -6,11 +6,13 @@
 				You are duplicating a report that is tied to the following 4-H Enrollment activity(-ies):
 			</p>
 			<p>
-				<ul class="activity-list">
-					<li v-for="activity in enrollmentActivities" :key="activity.ACTIVITY_ID">
-						{{ activity.ACTIVITY_NAME }} - ({{ activity.ACTIVITY_DATE | simple-date }})
-					</li>
-				</ul>
+			<ul class="activity-list">
+				<li v-for="activity in enrollmentActivities" :key="activity.ACTIVITY_ID">
+					{{ activity.ACTIVITY_NAME }} - ({{ simpleDate(activity.date) }})
+				</li>
+
+			</ul>
+
 			</p>
 			<p>
 				Check the "Demographic Information" checkbox to tie your new report to the same activity(-ies).
@@ -23,81 +25,91 @@
 </template>
 
 <script>
-	import { getAssociationReport4HEnrollmentActivity } from '~/modules/caesdb/gacounts3';
-	import {
-		getCriteriaStructure,
-		logError
-	} from '~/modules/caesdb';
+import { getAssociationReport4HEnrollmentActivity } from '~/modules/caesdb/gacounts3';
+import {
+	getCriteriaStructure,
+	logError
+} from '~/modules/caesdb';
 
-	export default {
-		name: 'DuplicationPanel4HEnrollment',
-		props: {
-			demographicsSelectedForImport: {
-				type: Boolean
-			}
-		},
-		data () {
-			const url = new URL(window.location.href);
-			const duplicateId = Number(url.searchParams.get('duplicateID'));
-
-			return {
-				enrollmentActivities: [],
-				duplicateId
-			};
-		},
-		computed: {
-			hasEnrollmentActivities () { return this.enrollmentActivities.length > 0; }
-		},
-		watch: {
-			demographicsSelectedForImport (val) {
-				if (val === true) this.$store.state['4HEnrollmentActivities'].records = [...this.enrollmentActivities];
-				else this.$store.state['4HEnrollmentActivities'].records = [];
-			}
-		},
-		mounted () {
-			getCriteriaStructure('GACOUNTS3', 'GC3_ASSOCIATION_REPORT_4H_ENROLLMENT_ACTIVITY', (err, data) => {
-				if (err) logError(new Error('Could not fetch criteria structure'));
-				if (!data) return;
-
-				const critStruct = { ...data };
-				critStruct.criteria_REPORT_ID_eq.push(this.duplicateId);
-				getAssociationReport4HEnrollmentActivity(critStruct, (subErr, subData) => {
-					if (subErr) logError(new Error('Could not fetch enrollment activity associations'));
-					if (!subData) return;
-
-					subData
-						// eslint-disable-next-line no-unused-vars
-						.map(({ REPORT_ID, DATE_CREATED, ...association }) => association)
-						.forEach(association => this.enrollmentActivities.push(association));
-				});
-			});
+export default {
+	name: 'DuplicationPanel4HEnrollment',
+	props: {
+		demographicsSelectedForImport: {
+			type: Boolean
 		}
-	};
+	},
+	data() {
+		const url = new URL(window.location.href);
+		const duplicateId = Number(url.searchParams.get('duplicateID'));
+
+		return {
+			enrollmentActivities: [],
+			duplicateId
+		};
+	},
+	computed: {
+		hasEnrollmentActivities() { return this.enrollmentActivities.length > 0; }
+	},
+	watch: {
+		demographicsSelectedForImport(val) {
+			if (val === true) this.$store.state['4HEnrollmentActivities'].records = [...this.enrollmentActivities];
+			else this.$store.state['4HEnrollmentActivities'].records = [];
+		}
+	},
+	methods: {
+		simpleDate(date) {
+			return dateFormat(date, 'm/d/yy');
+		}
+	},
+	mounted() {
+		getCriteriaStructure('GACOUNTS3', 'GC3_ASSOCIATION_REPORT_4H_ENROLLMENT_ACTIVITY', (err, data) => {
+			if (err) logError(new Error('Could not fetch criteria structure'));
+			if (!data) return;
+
+			const critStruct = { ...data };
+			critStruct.criteria_REPORT_ID_eq.push(this.duplicateId);
+			getAssociationReport4HEnrollmentActivity(critStruct, (subErr, subData) => {
+				if (subErr) logError(new Error('Could not fetch enrollment activity associations'));
+				if (!subData) return;
+
+				subData
+					// eslint-disable-next-line no-unused-vars
+					.map(({ REPORT_ID, DATE_CREATED, ...association }) => association)
+					.forEach(association => this.enrollmentActivities.push(association));
+			});
+		});
+	}
+};
 </script>
 
 <style lang="scss" scoped>
-		div.pane-wrapper-wrapper {
-			background: #eee;
-			padding: 1rem 0;
-			margin-bottom: 1rem;
-			div.pane-wrapper {
-				margin: 0 2rem;
-			}
-		}
+div.pane-wrapper-wrapper {
+	background: #eee;
+	padding: 1rem 0;
+	margin-bottom: 1rem;
 
-	ul {
-		&.activity-list { padding: 0 1rem; }
+	div.pane-wrapper {
+		margin: 0 2rem;
+	}
+}
 
-		&.disabled-text {
-			color: #aaa;
-			text-decoration: line-through;
-			font-style: italic;
-		}
+ul {
+	&.activity-list {
+		padding: 0 1rem;
 	}
 
-	p.four-h-logo-wrapper {
-		text-align: center;
-
-		img { width: 7rem; }
+	&.disabled-text {
+		color: #aaa;
+		text-decoration: line-through;
+		font-style: italic;
 	}
+}
+
+p.four-h-logo-wrapper {
+	text-align: center;
+
+	img {
+		width: 7rem;
+	}
+}
 </style>
